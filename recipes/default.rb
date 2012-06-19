8688369d230289b6ecdc00ef716b02f4b0808aef
Thorsten Fischer <thorsten@froschi.org>
2012-06-19 15:08:59 +0200

    Created the splitup into 1.8 and 1.9.1 packages.
    This is a very Ubuntu-specific experiment. Looking forward
    to see how it will laugh at me on Fedora.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,1 @@
-packages = Array.new
-
-case node[:lsb][:codename]
-when "lucid", "precise"
-  packages |= %w/
-    libruby1.8
-  /
-end
-
-packages.each do |pkg|
-  package pkg do
-    action [:install, :upgrade]
-  end
-end
+include_recipe "libruby::1.8"
