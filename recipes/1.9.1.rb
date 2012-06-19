470ea3dd1e15f5f84908e80cfa0209cc188e38fa
Thorsten Fischer <thorsten@froschi.org>
2012-06-19 15:23:09 +0200

    Added dependencies for libruby::1.9.1.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/1.9.1.rb
+++ recipes/1.9.1.rb
@@ -2,10 +2,20 @@ packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libncurses"
+
   packages |= %w/
     libruby1.9.1
   /
 when "precise"
+  include_recipe "libffi"
+  include_recipe "libgdbm"
+  include_recipe "libncursesw"
+  include_recipe "libreadline"
+  include_recipe "libssl"
+  include_recipe "libtinfo"
+  include_recipe "libyaml"
+
   packages |= %w/
     libruby1.9.1
   /
